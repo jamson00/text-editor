383793684b0a282be1a99daaeae11a133a41a20a
jamson00 <jameson.edward.daley@gmail.com>
2023-09-19 13:48:48 -0400

    setup backend, create dbPromise variablen define getDb function and putDb function

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const dbPromise = initdb();
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const newItem = { content, timestamp: Date.now() };
 
-initdb();
+  await store.add(newItem);
+
+  console.log('Added to the database:', newItem);
+};
+
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  const allContent = await store.getAll();
+
+  console.log('Retrieved from the database:', allContent);
+
+  return allContent;
+};
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Middleware for handling URL-encoded and JSON requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Include your routes here, assuming you have an 'htmlRoutes.js' file in a 'routes' directory
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+// Error handling middleware (custom or use a library like 'express-error-handler')
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
